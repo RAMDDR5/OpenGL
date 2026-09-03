db69f23eb8b81bbc28d93b2e6c2025e43b08ecab
RAMDDR5 <bugthemonke@gmail.com>
2026-09-03 19:25:54 +0200

    added 3d

--- main.c
+++ main.c
@@ -1,7 +1,12 @@
 #include <glad/gl.h>
 #include <GLFW/glfw3.h>
+#include <cglm/cglm.h>
 
-#include "headers/shapes.h"
+#include "cglm/affine-pre.h"
+#include "cglm/cam.h"
+#include "cglm/mat4.h"
+//#include "headers/shapes.h"
+#include "cglm/util.h"
 #include "headers/shader.h"
 #include "headers/VBO.h"
 #include "headers/EBO.h"
@@ -32,21 +37,52 @@ int main(void) {
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 	GLfloat vertices[] = {
-		SQUARE_TOP_LEFT,		CHLORINE_BLUE,		0.0f, 1.0f,
-		SQUARE_TOP_RIGHT,		KREMA_PURPLE,		1.0f, 1.0f,
-		SQUARE_BOTTOM_RIGHT,	KREMA_ORANGE,		1.0f, 0.0f,
-		SQUARE_BOTTOM_LEFT,		KREMA_GREEN,		0.0f, 0.0f
+		// Front face (+Z)
+		-0.5f, -0.5f,  0.5f,		CHLORINE_BLUE,		0.0f, 0.0f,
+		 0.5f, -0.5f,  0.5f,		CHLORINE_BLUE,		1.0f, 0.0f,
+		 0.5f,  0.5f,  0.5f,		CHLORINE_BLUE,		1.0f, 1.0f,
+		-0.5f,  0.5f,  0.5f,		CHLORINE_BLUE,		0.0f, 1.0f,
+		// Back face (-Z)
+		 0.5f, -0.5f, -0.5f,		KREMA_PURPLE,		0.0f, 0.0f,
+		-0.5f, -0.5f, -0.5f,		KREMA_PURPLE,		1.0f, 0.0f,
+		-0.5f,  0.5f, -0.5f,		KREMA_PURPLE,		1.0f, 1.0f,
+		 0.5f,  0.5f, -0.5f,		KREMA_PURPLE,		0.0f, 1.0f,
+		// Right face (+X)
+		 0.5f, -0.5f,  0.5f,		KREMA_ORANGE,		0.0f, 0.0f,
+		 0.5f, -0.5f, -0.5f,		KREMA_ORANGE,		1.0f, 0.0f,
+		 0.5f,  0.5f, -0.5f,		KREMA_ORANGE,		1.0f, 1.0f,
+		 0.5f,  0.5f,  0.5f,		KREMA_ORANGE,		0.0f, 1.0f,
+		// Left face (-X)
+		-0.5f, -0.5f, -0.5f,		KREMA_GREEN,		0.0f, 0.0f,
+		-0.5f, -0.5f,  0.5f,		KREMA_GREEN,		1.0f, 0.0f,
+		-0.5f,  0.5f,  0.5f,		KREMA_GREEN,		1.0f, 1.0f,
+		-0.5f,  0.5f, -0.5f,		KREMA_GREEN,		0.0f, 1.0f,
+		// Top face (+Y)
+		-0.5f,  0.5f,  0.5f,		PURE_WHITE,			0.0f, 0.0f,
+		 0.5f,  0.5f,  0.5f,		PURE_WHITE,			1.0f, 0.0f,
+		 0.5f,  0.5f, -0.5f,		PURE_WHITE,			1.0f, 1.0f,
+		-0.5f,  0.5f, -0.5f,		PURE_WHITE,			0.0f, 1.0f,
+		// Bottom face (-Y)
+		-0.5f, -0.5f, -0.5f,		PURE_BLACK,			0.0f, 0.0f,
+		 0.5f, -0.5f, -0.5f,		PURE_BLACK,			1.0f, 0.0f,
+		 0.5f, -0.5f,  0.5f,		PURE_BLACK,			1.0f, 1.0f,
+		-0.5f, -0.5f,  0.5f,		PURE_BLACK,			0.0f, 1.0f,
 	};
 
 	GLuint indices[] = {
-		0, 1, 2,
-		0, 3, 2
+		 0,  1,  2,  2,  3,  0,
+		 4,  5,  6,  6,  7,  4,
+		 8,  9, 10, 10, 11,  8,
+		12, 13, 14, 14, 15, 12,
+		16, 17, 18, 18, 19, 16,
+		20, 21, 22, 22, 23, 20
 	};
-
+	
 	GLFWwindow *window = glfwCreateWindow(800, 800, "ObamaGL", NULL, NULL);
 	int windowHeight, windowWidth;
 
 	glfwMakeContextCurrent(window);
+
 	gladLoadGL(glfwGetProcAddress);
 
 	Shader shaderProgram = CreateShader("headers/default.vert", "headers/default.frag");
@@ -61,14 +97,21 @@ int main(void) {
 	LinkAttrib(VBO1, 0, 3, GL_FLOAT, 8 * sizeof(float), (void*)0);
 	LinkAttrib(VBO1, 1, 3, GL_FLOAT, 8 * sizeof(float), (void*)(3 * sizeof(float)));
 	LinkAttrib(VBO1, 2, 2, GL_FLOAT, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+	
 	UnbindVAO();
 	UnbindVBO();
 	UnbindEBO();
 
 	GLuint uniID = glGetUniformLocation(shaderProgram.ID, "scale"); 
 
-	Texture imageTexture = CreateTexture("image.png", GL_TEXTURE_2D, GL_TEXTURE0, GL_RGBA, GL_UNSIGNED_BYTE, GL_LINEAR);	
+	Texture imageTexture = CreateTexture("image.png", GL_TEXTURE_2D, GL_TEXTURE0, GL_RGBA, GL_UNSIGNED_BYTE, GL_LINEAR);
+
 	texUnit(shaderProgram, "tex0", 0);
+
+	float rotation = 0.0f;
+	double prevTime = glfwGetTime();
+
+	glEnable(GL_DEPTH_TEST);
 
 	while (!glfwWindowShouldClose(window)) {
 		glfwPollEvents();
@@ -76,17 +119,46 @@ int main(void) {
 		glfwGetWindowSize(window, &windowWidth, &windowHeight);
 		glViewport(0, 0, windowWidth, windowHeight);
 
+
 		ActivateShader(shaderProgram);
+
+		double crntTime = glfwGetTime();
+		if (crntTime - prevTime >= 1 / 60) {
+			rotation += 0.5f;
+			prevTime = crntTime;
+		}
+
+		mat4 model;
+		mat4 view;
+		mat4 proj;
+
+		glm_mat4_identity(model);
+		glm_mat4_identity(view);
+		glm_mat4_identity(proj);
+
+		glm_rotate(model, glm_rad(rotation), (vec3){0.0f, 1.0f, 0.0f});
+		glm_translate(view, (vec3){0.0, -0.5f, -6.0});
+		glm_perspective(glm_rad(45.0f), (float)windowWidth/windowHeight, 0.1f, 100.0f, proj);
+
+		int modelLoc = glGetUniformLocation(shaderProgram.ID, "model");
+		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, (float*)model);
+
+		int viewLoc = glGetUniformLocation(shaderProgram.ID, "view");
+		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, (float*)view);
+
+		int projLoc = glGetUniformLocation(shaderProgram.ID, "proj");
+		glUniformMatrix4fv(projLoc, 1, GL_FALSE, (float*)proj);
+
+
 		glUniform1f(uniID, 0.5f);
 		BindTexture(imageTexture);
 
 		BindVAO(VAO1);
 	
 		glClearColor(PURE_BLACK, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		glDrawElements(GL_TRIANGLES, sizeof(indices)/sizeof(GLuint), GL_UNSIGNED_INT, 0);
-
 		glfwSwapBuffers(window);
 	}
 
