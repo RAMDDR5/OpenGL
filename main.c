ff1e93609bb9660c949dc423800487fdfc43de03
RAMDDR5 <bugthemonke@gmail.com>
2026-09-03 15:46:24 +0200

    added textures to main.c

--- main.c
+++ main.c
@@ -31,10 +31,10 @@ int main(void) {
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 	GLfloat vertices[] = {
-		SQUARE_TOP_LEFT,		CHLORINE_BLUE,
-		SQUARE_TOP_RIGHT,		KREMA_PURPLE,
-		SQUARE_BOTTOM_RIGHT,	KREMA_ORANGE,
-		SQUARE_BOTTOM_LEFT,		KREMA_GREEN
+		SQUARE_TOP_LEFT,		CHLORINE_BLUE,		0.0f, 1.0f,
+		SQUARE_TOP_RIGHT,		KREMA_PURPLE,		1.0f, 1.0f,
+		SQUARE_BOTTOM_RIGHT,	KREMA_ORANGE,		1.0f, 0.0f,
+		SQUARE_BOTTOM_LEFT,		KREMA_GREEN,		0.0f, 0.0f
 	};
 
 	GLuint indices[] = {
@@ -57,13 +57,40 @@ int main(void) {
 	VBO VBO1 = CreateVBO(vertices, sizeof(vertices));
 	EBO EBO1 = CreateEBO(indices, sizeof(indices));
 
-	LinkAttrib(VBO1, 0, 3, GL_FLOAT, 6 * sizeof(float), (void*)0);
-	LinkAttrib(VBO1, 1, 3, GL_FLOAT, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	LinkAttrib(VBO1, 0, 3, GL_FLOAT, 8 * sizeof(float), (void*)0);
+	LinkAttrib(VBO1, 1, 3, GL_FLOAT, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+	LinkAttrib(VBO1, 2, 2, GL_FLOAT, 8 * sizeof(float), (void*)(6 * sizeof(float)));
 	UnbindVAO();
 	UnbindVBO();
 	UnbindEBO();
 
 	GLuint uniID = glGetUniformLocation(shaderProgram.ID, "scale"); 
+
+	int imageWidth, imageHeight, numColCh;
+	stbi_set_flip_vertically_on_load(1);
+	unsigned char *bytes = stbi_load("image.png", &imageWidth, &imageHeight, &numColCh, 0);
+
+	GLuint texture;
+	glGenTextures(1, &texture);
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, texture);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, imageWidth, imageHeight, 0, GL_RGBA, GL_UNSIGNED_BYTE, bytes);
+	glGenerateMipmap(GL_TEXTURE_2D);
+
+	stbi_image_free(bytes);
+	glBindTexture(GL_TEXTURE_2D, 0);
+
+	GLuint tex0Uni = glGetUniformLocation(shaderProgram.ID, "tex0");
+	ActivateShader(shaderProgram);
+
+	glUniform1i(tex0Uni, 0);
 
 	while (!glfwWindowShouldClose(window)) {
 		glfwPollEvents();
@@ -73,6 +100,7 @@ int main(void) {
 
 		ActivateShader(shaderProgram);
 		glUniform1f(uniID, 0.5f);
+		glBindTexture(GL_TEXTURE_2D, texture);
 
 		BindVAO(VAO1);
 	
@@ -88,6 +116,7 @@ int main(void) {
 	DeleteVBO(VBO1);
 	DeleteEBO(EBO1);
 	DeleteShader(shaderProgram);
+	glDeleteTextures(1, &texture);
 
 	glfwDestroyWindow(window); 
 	glfwTerminate();
